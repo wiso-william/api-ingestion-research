0d428ad204ab4edd8c1335b8bbae24d17c61bcda
William Mancy <w.mancy@med-ea.it>
2026-08-27 10:54:29 +0200

    Api extraction handles http errors and retries

--- src/api_ingestion_research/ingestion/api.py
+++ src/api_ingestion_research/ingestion/api.py
@@ -1,7 +1,10 @@
 import requests
+import tenacity
 
 import logging 
 from typing import Any
+
+from src.api_ingestion_research.utils.error_handling import is_retriable_http_error
 
 
 
@@ -11,6 +14,13 @@ logger = logging.getLogger(__name__)
 URL = "https://dummyjson.com/products"
 
 
+
+@tenacity.retry(
+        tenacity.retry_if_exception(is_retriable_http_error), # Dico quali codici mi interessano
+        stop=tenacity.stop_after_attempt(10), 
+        wait=tenacity.wait_exponential(multiplier=1,min=1,max=10), # Exponential Backoff
+        reraise=True # Non mi perdo l'errore e lo raisa se fallisce 
+)
 def extract_products(limit: int, skip_amount: int):
     response = requests.get(
         URL,
